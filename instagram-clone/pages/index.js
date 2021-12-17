93126eb3f4e992e2559154d3d0fbfc2e45ca1b6e
devhabeeblateef <77229868+devhabeeblateef@users.noreply.github.com>
2021-12-17 03:34:39 +0100

    Ready to build the header:)

---
+++ instagram-clone/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0 Build</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+<h1>This is the Instagram 2.0 Build</h1>
+     {/* Header */}
+
+       {/* Feed */}
+
+       {/* Modal */}
+    </div>
+  )
+}
