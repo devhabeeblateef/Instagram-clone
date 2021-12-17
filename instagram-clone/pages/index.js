6e411d24f86577d82b61e371e991e3cf7fbd7424
devhabeeblateef <77229868+devhabeeblateef@users.noreply.github.com>
2021-12-17 18:20:28 +0100

    Done with Header, moving to Feed :)

--- instagram-clone/pages/index.js
+++ instagram-clone/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import  Header from "../components/Header"
 export default function Home() {
   return (
     <div className="">
@@ -7,9 +7,9 @@ export default function Home() {
         <title>Instagram 2.0 Build</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<h1>This is the Instagram 2.0 Build</h1>
+    
      {/* Header */}
-
+        <Header/>
        {/* Feed */}
 
        {/* Modal */}
